946e8020a71e86681233ff0b7914f3adfb9e7bdb
Luke Geneva <lukegeneva@gmail.com>
2019-11-17 19:42:18 -0600

    Enable touch backend for mobile

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { DndProvider } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
+import TouchBackend from 'react-dnd-touch-backend';
 import './App.css';
 import Clock from './Clock';
 import Roster from './Roster';
 
 function App() {
+  const hasTouch = navigator.maxTouchPoints > 0;
+  const backend = hasTouch ? TouchBackend : HTML5Backend;
+
   return (
     <div className="App">
-      <DndProvider backend={HTML5Backend}>
+      <DndProvider backend={backend}>
         <Clock />
         <Roster />
       </DndProvider>
